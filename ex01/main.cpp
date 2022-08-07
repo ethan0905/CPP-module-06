93b6fb73e32e5b8200ca859ebb99ac30b11ec9c9
Safar Ethan <ethansafar@gmail.com>
2022-08-07 19:36:41 +0200

    retinterpret cast for conversion ptr to raw, then raw_casted to ptr_casted looks to be working well

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: c2h6 <c2h6@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 18:16:02 by c2h6              #+#    #+#             */
-/*   Updated: 2022/08/07 19:17:44 by c2h6             ###   ########.fr       */
+/*   Updated: 2022/08/07 19:32:18 by c2h6             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ struct Data
 {
 	char c;
 	int	n;
-	float f;
-	double d;
 };
 
 uintptr_t serialize(Data* ptr) 
@@ -41,11 +39,23 @@ int	main(int ac, char **av)
 	(void)av;
 	if (ac == 1)
 	{
-		Data	*data = NULL;
+		Data	ptr;
+		Data	*ptr_casted;
 		uintptr_t	raw = 0;
+
+		ptr.c = 'A';
+		ptr.n = 42;
 		
-		serialize(data);
-		deserialize(raw);
+		std::cout << "data ptr:" << std::endl; 
+		std::cout << "ptr.c: '" << ptr.c << "'" << std::endl; 
+		std::cout << "ptr.n: " << ptr.n << "" << std::endl; 
+
+		raw = serialize(&ptr);
+		ptr_casted = deserialize(raw);
+
+		std::cout << "data ptr_casted:" << std::endl; 
+		std::cout << "ptr_casted->c: '" << ptr_casted->c << "'" << std::endl; 
+		std::cout << "ptr_casted->n: " << ptr_casted->n << "" << std::endl; 
 	}
 	else
 		std::cerr << "Error: invalid synthax. Try with ./execName <value>" << std::endl;
