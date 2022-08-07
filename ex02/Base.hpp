f274244f3216a7ae016744dd608f831ce70c4fc1
Safar Ethan <ethansafar@gmail.com>
2022-08-07 19:56:55 +0200

    initialized files + Base class

---
+++ ex02/Base.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: c2h6 <c2h6@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/07 19:52:17 by c2h6              #+#    #+#             */
+/*   Updated: 2022/08/07 19:56:13 by c2h6             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+# define BASE_HPP
+
+#include <iostream>
+#include "color.h"
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+class Base {
+    
+    public:
+        virtual ~Base( void );
+        
+    private:
+};
+
+#endif
