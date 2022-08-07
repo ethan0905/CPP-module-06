5c73e5b4ff4eac8b146624fd01b643e53d6b98e8
Safar Ethan <ethansafar@gmail.com>
2022-08-07 20:46:16 +0200

    cleaned main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: c2h6 <c2h6@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 18:16:02 by c2h6              #+#    #+#             */
-/*   Updated: 2022/08/07 20:45:31 by c2h6             ###   ########.fr       */
+/*   Updated: 2022/08/07 20:46:04 by c2h6             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int ac, char **av)
 {
 	(void)av;
+
 	if (ac == 1)
 	{
 		Base	*base = NULL;
@@ -27,5 +28,6 @@ int	main(int ac, char **av)
 	}
 	else
 		std::cerr << "Error: invalid synthax. Try with ./execName" << std::endl;
+
 	return (0);
 }
