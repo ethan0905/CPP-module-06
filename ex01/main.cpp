77299b27d5f557f6adbc1a8e4cb67e94d1474be1
Safar Ethan <ethansafar@gmail.com>
2022-08-07 19:44:56 +0200

    cleaned output, added some colors

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,11 +6,12 @@
 /*   By: c2h6 <c2h6@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 18:16:02 by c2h6              #+#    #+#             */
-/*   Updated: 2022/08/07 19:32:18 by c2h6             ###   ########.fr       */
+/*   Updated: 2022/08/07 19:44:06 by c2h6             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "color.hpp"
 
 struct Data
 {
@@ -46,16 +47,29 @@ int	main(int ac, char **av)
 		ptr.c = 'A';
 		ptr.n = 42;
 		
-		std::cout << "data ptr:" << std::endl; 
+		std::cout << CYAN "======= Before Serialisation ========" END << std::endl; 
+				
+		std::cout << YELLOW "data ptr:" << std::endl; 
 		std::cout << "ptr.c: '" << ptr.c << "'" << std::endl; 
-		std::cout << "ptr.n: " << ptr.n << "" << std::endl; 
+		std::cout << "ptr.n: " << ptr.n << "" END << std::endl; 
 
 		raw = serialize(&ptr);
 		ptr_casted = deserialize(raw);
 
-		std::cout << "data ptr_casted:" << std::endl; 
+		std::cout << CYAN "======== After Serialisation ========" END << std::endl; 
+
+		std::cout << MAGENTA "data ptr_casted:" << std::endl; 
 		std::cout << "ptr_casted->c: '" << ptr_casted->c << "'" << std::endl; 
-		std::cout << "ptr_casted->n: " << ptr_casted->n << "" << std::endl; 
+		std::cout << "ptr_casted->n: " << ptr_casted->n << "" END << std::endl;
+
+		ptr_casted->c = 'z';
+		ptr_casted->n = 21;
+
+		std::cout << CYAN "======== After Use of Struct ========" END << std::endl; 
+
+		std::cout << WHITE "data ptr_casted:" << std::endl; 
+		std::cout << "ptr_casted->c: '" << ptr_casted->c << "'" << std::endl; 
+		std::cout << "ptr_casted->n: " << ptr_casted->n << "" END << std::endl;
 	}
 	else
 		std::cerr << "Error: invalid synthax. Try with ./execName <value>" << std::endl;
