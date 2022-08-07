3134789671aa0adc34819868d4a17e1f2efb1f1e
Safar Ethan <ethansafar@gmail.com>
2022-08-07 20:45:47 +0200

    identifying reference is ok

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: c2h6 <c2h6@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 19:52:13 by c2h6              #+#    #+#             */
-/*   Updated: 2022/08/07 20:27:24 by c2h6             ###   ########.fr       */
+/*   Updated: 2022/08/07 20:44:38 by c2h6             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,8 +61,38 @@ void    Base::identify( Base *p ) {
 	return ;
 }
 
-// void    Base::identify( Base &p ) {
+void    Base::identify( Base &p ) {
 
-// 	return ;
-// }
+	try
+	{
+		A	a = dynamic_cast<A&>(p);
+		std::cout << YELLOW "Class type is [A]" END << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED "Error: " << e.what() << ": Class type is not [A]" END << std::endl;
+	}
+	
+	try
+	{
+		B	b = dynamic_cast<B&>(p);
+		std::cout << MAGENTA "Class type is [B]" END << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED "Error: " << e.what() << ": Class type is not [B]" END << std::endl;
+	}
+
+	try
+	{
+		C	c = dynamic_cast<C&>(p);
+		std::cout << WHITE "Class type is [C]" END << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED "Error: " << e.what() << ": Class type is not [C]" END << std::endl;
+	}
+
+	return ;
+}
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: c2h6 <c2h6@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 18:16:02 by c2h6              #+#    #+#             */
-/*   Updated: 2022/08/07 20:31:12 by c2h6             ###   ########.fr       */
+/*   Updated: 2022/08/07 20:45:31 by c2h6             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	main(int ac, char **av)
 		
 		base = base->generate();
 		base->identify(base);
-		
+		base->identify(*base);
+
 		delete (base);
 	}
 	else
